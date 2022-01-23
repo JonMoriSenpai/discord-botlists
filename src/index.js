8325a0e13fd4cc1e7334c3ca83fa3e64b34b9659
SidisLiveYT <SidisLiveYT@users.noreply.github.com>
2022-01-23 15:00:01 +0000

    deploy: 9892c50a7f79017bc2ebee756bd6e4e65042a089

--- src/index.js
+++
@@ -1,7 +0,0 @@
-const BotLists = require('./Handlers/Request.js')
-const botlistJson = require('./resources/botlists.json')
-
-module.exports = {
-  BotLists,
-  botlistJson,
-}
--- src/index.mjs
+++
@@ -1,5 +0,0 @@
-import Requests from './index'
-
-export default Requests
-export const BotLists = Requests.BotLists
-export const botlistJson = Requests.botlistJson
